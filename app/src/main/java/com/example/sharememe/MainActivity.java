7815b8fc5c2614b28062472b22ca09f48ee9729f
Vishal Kumar Rai <7376vishalkumarrai@gmail.com>
2021-06-04 13:39:36 +0530

    HasteRaho

---
+++ app/src/main/java/com/example/sharememe/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.sharememe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DownloadManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        load();
+    }
+    private void load()
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://meme-api.herokuapp.com/gimme";
+        StringRequest request=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject=new JSONObject(response.toString());
+                    String a=jsonObject.getString("title");
+                    Log.d("title",a);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
+
+    public void shareMeme(View view) {
+    }
+
+    public void nextMeme(View view) {
+    }
+}
